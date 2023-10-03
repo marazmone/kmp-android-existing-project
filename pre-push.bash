bbfa49a7fd39e9a782a7a6c72c5c6e86f3708ae8
Serhii Hulenko <sergei.gulenko@gmail.com>
2023-10-03 19:38:49 +0200

    update libs

--- pre-push.bash
+++ pre-push.bash
@@ -2,6 +2,6 @@
 echo "- Build KmpAndroidExistingProjectShared -"
 ./gradlew shared:createXCFramework
 if [ $? -ne 0 ]; then
- echo "Km pAndroidExistingProjectShare building failed."
+ echo "KmpAndroidExistingProjectShare building failed."
  exit 1
 fi
